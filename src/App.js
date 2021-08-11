6d571a58054541f70c1e8c9fb172f61e195c69e9
seokwoo-park <p.seokwoo@gmail.com>
2021-08-12 01:17:34 +0900

    -

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { TopBar, Home, Single, Write, Settings  } from './components/index.js'
+
+
+function App() {
+  return (
+    <>
+      <TopBar/>
+      <Settings/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/topbar/TopBar.jsx
@@ -0,0 +1,31 @@
+import './topbar.css'
+
+export default function TopBar() {
+    return (
+        <div className="top">
+            <div className="topLeft">
+                <i className="topIcon fab fa-facebook-square"></i>
+                <i className="topIcon fab fa-twitter-square"></i>
+                <i className="topIcon fab fa-instagram-square"></i>
+                <i className="topIcon fab fa-pinterest-square"></i>
+            </div>
+            <div className="topCenter">
+                <ul className="topList">
+                    <li className="topListItem">HOME</li>
+                    <li className="topListItem">ABOUT</li>
+                    <li className="topListItem">CONTACT</li>
+                    <li className="topListItem">WRITE</li>
+                    <li className="topListItem">LOG OUT</li>
+                </ul>
+            </div>
+            <div className="topRight">
+                <img
+                className="topImg"
+                src="https://wallpapercave.com/wp/wp6196631.jpg"
+                alt=""
+                />
+                <i className="topSearchIcon fas fa-search"></i>
+            </div>
+        </div>
+    )
+}
